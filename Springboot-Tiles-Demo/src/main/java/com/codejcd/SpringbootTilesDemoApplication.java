5f94a1d783f3e931b8ee145f6b0ce22df314a2c9
codejcd <codejcd@gmail.com>
2020-07-02 22:33:44 +0900

    init commit

---
+++ Springboot-Tiles-Demo/src/main/java/com/codejcd/SpringbootTilesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codejcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTilesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTilesDemoApplication.class, args);
+	}
+
+}
---
+++ Springboot-Tiles-Demo/src/main/webapp/WEB-INF/views/common/header.jsp
@@ -0,0 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<div>
+	<h3>HEADER!</h3>
+</div>
